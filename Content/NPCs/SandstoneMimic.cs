acc7274f9f84e80c7b20e5f2974d4515a419ce8e
YourFriendSilvie <silver.swift@outlook.com>
2024-06-19 16:16:55 -0700

    Co-authored-by: gabeswift@outlook.com <gabeswift@outlook.com>

---
+++ Content/NPCs/SandstoneMimic.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MimicsPlus.Content.NPCs.SandstoneMimic
+{
+    public class SandstoneMimic : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Sandstone Mimic");
+            npc.width = 16;
+            npc.height = 16;
+            npc.aiStyle = NPCAiStyle.Mimic;
+            npc.damage = 30;
+            npc.lifeMax = 300;
+                    
+        }
+    }
+}
