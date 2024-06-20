dbab44c474dd5b2054bb4d16cabc3002a463e3b6
YourFriendSilvie <silver.swift@outlook.com>
2024-06-19 18:44:16 -0700

    Sandstone Mimic base code
    Co-authored-by: gabeswift@outlook.com <gabeswift@outlook.com>

--- Content/NPCs/SandstoneMimic.cs
+++ Content/NPCs/SandstoneMimic.cs
@@ -1,20 +1,58 @@
+using System.Collections.Generic;
 using Terraria;
+using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
 
 namespace MimicsPlus.Content.NPCs.SandstoneMimic
 {
     public class SandstoneMimic : ModNPC
     {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = Main.npcFrameCount[NPCID.Mimic];
+
+
+        }
         public override void SetDefaults()
         {
-            DisplayName.SetDefault("Sandstone Mimic");
-            npc.width = 16;
-            npc.height = 16;
-            npc.aiStyle = NPCAiStyle.Mimic;
-            npc.damage = 30;
-            npc.lifeMax = 300;
-            npc.defense = 12;
+            NPC.width = 24;
+            NPC.height = 24;
+            NPC.damage = 80;
+            NPC.defense = 30;
+            NPC.lifeMax = 500;
+            NPC.HitSound = SoundID.NPCHit4;
+            NPC.DeathSound = SoundID.NPCDeath6;
+            NPC.value = 100000f;
+            NPC.knockBackResist = 0.7f;
+            NPC.aiStyle = 25;
+            NPC.rarity = 4;
+
+            AIType = NPCID.Mimic;
+            AnimationType = NPCID.Mimic;
+            
+        }
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+			return SpawnCondition.DesertCave.Chance * SpawnCondition.UndergroundMimic.Chance;
+		}
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            IItemDropRule[] primaryDrops = [
+            ItemDropRule.Common(ItemID.MagicConch, 7, 1, 1),
+            ItemDropRule.Common(ItemID.MysticCoilSnake, 7, 1, 1),
+            ItemDropRule.Common(ItemID.SandBoots, 7, 1, 1),
+            ItemDropRule.Common(ItemID.AncientChisel, 7, 1, 1),
+            ItemDropRule.Common(ItemID.CatBast, 7, 1, 1) //Bast Statue
+            ]; 
+            npcLoot.Add(new OneFromRulesRule(1, primaryDrops));
+
+            IItemDropRule[] secondaryDrops = [
+                ItemDropRule.Common(ItemID.DesertMinecart, 15, 1, 1), ItemDropRule.Common(ItemID.UncumberingStone, 7, 1, 1)
+            ];
+            npcLoot.Add(new OneFromRulesRule(1, secondaryDrops));
         }
     }
 }
