66b59ab035434bff1f46e2c9c54578118a62bf95
TNTiger77 <71955426+TNTiger77@users.noreply.github.com>
2024-06-20 19:27:21 -0700

    Swarmer buff

---
+++ Content/Buffs/AntlionSwarmerBuff.cs
@@ -0,0 +1,25 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MimicsPlus.Content.Buffs
+{
+    public class AntlionSwarmerBuff : ModBuff
+    {
+        public override void SetStaticDefaults() {
+			Main.buffNoSave[Type] = true; // This buff won't save when you exit the world
+			Main.buffNoTimeDisplay[Type] = true; // The time remaining won't display on this buff
+		}
+
+		public override void Update(Player player, ref int buffIndex) {
+			// If the minions exist reset the buff time, otherwise remove the buff from the player
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<AntlionSwarmerMinion>()] > 0) {
+				player.buffTime[buffIndex] = 18000;
+			}
+			else {
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+		}
+    }
+}
